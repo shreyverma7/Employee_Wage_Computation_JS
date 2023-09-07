a51d6cbc7e0d9631370bb772540273bbe326fe5d
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-09-07 12:57:36 +0530

    [Shrey] Add : Check employee present or Absent

--- Employee_Wage_Computation_JS/EmpWage.js
+++ Employee_Wage_Computation_JS/EmpWage.js
@@ -1,1 +1,12 @@
 console.log("Employee_Wage_Computiation_Problems");
+//UC1
+const IS_AbBSENT=0;
+let empcheck = Math.floor(Math.random()*10)%2;
+if(empcheck==IS_AbBSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+console.log("Employee is Present");
+}
