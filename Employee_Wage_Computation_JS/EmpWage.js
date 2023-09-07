822d1ccd411cedc0679b044f4dddbdfd4b6faa1e
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-09-07 13:06:55 +0530

    [Shrey] Add : calculate the wage for a month

--- Employee_Wage_Computation_JS/EmpWage.js
+++ Employee_Wage_Computation_JS/EmpWage.js
@@ -4,6 +4,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HR=4;
 const Full_TIME_HR=8;
 const WAGE_PER_HR=20;
+const NUM_OF_WORKING_DAYS=20;
 
 let empcheck = Math.floor(Math.random()*10)%2;
 if(empcheck==IS_AbBSENT)
@@ -26,7 +27,7 @@ switch(empcheck)
     case IS_AbBSENT: emphrs=IS_AbBSENT
 }
 let empwage= emphrs*WAGE_PER_HR;
-console.log("Employee Wage "+empwage);
+console.log(empwage);
 
 //UC3
 function getworkinghrs()
@@ -45,4 +46,14 @@ let totalemphrs=0;
 empcheck=Math.floor(Math.random()*10)%3;
 totalemphrs=getworkinghrs(empcheck);
 let totalempwage=emphrs*WAGE_PER_HR;
-console.log("Total Employee Wages hours "+totalempwage);
+console.log(totalempwage);
+//UC4
+let  hrs=0;
+for(let i=0;i<NUM_OF_WORKING_DAYS;i++)
+{
+    empcheck=Math.floor(Math.random()*10)%3;
+    hrs+=getworkinghrs(empcheck);
+}
+totalempwage=hrs*WAGE_PER_HR;
+console.log(totalempwage);
+//UC5
