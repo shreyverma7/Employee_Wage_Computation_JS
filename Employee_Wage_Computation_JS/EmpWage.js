c8cd0fe18025dd6ad03e42015299136454643ada
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-09-07 13:00:32 +0530

    [Shrey] Add :Calcultate Daily Employee Wage

--- Employee_Wage_Computation_JS/EmpWage.js
+++ Employee_Wage_Computation_JS/EmpWage.js
@@ -1,6 +1,12 @@
 console.log("Employee_Wage_Computiation_Problems");
 //UC1
 const IS_AbBSENT=0;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HR=4;
+const Full_TIME_HR=8;
+const WAGE_PER_HR=20;
+
 let empcheck = Math.floor(Math.random()*10)%2;
 if(empcheck==IS_AbBSENT)
 {
@@ -10,3 +16,16 @@ else
 {
 console.log("Employee is Present");
 }
+//UC2
+let emphrs=0;
+empcheck=Math.floor(Math.random()*10)%3;
+switch(empcheck)
+{
+    case IS_PART_TIME: emphrs=PART_TIME_HR;
+    break;
+    case IS_FULL_TIME: emphrs=Full_TIME_HR;
+    break;
+    case IS_AbBSENT: emphrs=IS_AbBSENT
+}
+let empwage= emphrs*WAGE_PER_HR;
+console.log(empwage);
